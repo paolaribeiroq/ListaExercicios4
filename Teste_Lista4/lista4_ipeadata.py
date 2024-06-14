199ea7e2f0f436dd342ab0de088cc3114e3cfafd
Paola Ribeiro <paolacrsilva@gmail.com>
2024-06-14 20:22:23 -0300

    Update lista4_ipeadata.py

--- Teste_Lista4/lista4_ipeadata.py
+++ Teste_Lista4/lista4_ipeadata.py
@@ -48,8 +48,7 @@ st.pyplot(fig)
 st.code(code, language='python')
 
 fig, ax = plt.subplots()
-ip.timeseries('BM12_TJOVER12', year=2021).plot("MONTH", "VALUE ((% a.m.)), ax=ax")
-ip.timeseries('BM12_TJOVER12', year=2022).plot("MONTH", "VALUE ((% a.m.)), ax=ax")
+selic(year=2021).plot("MONTH", "VALUE ((% a.m.)), ax=ax")
 st.pyplot(fig)
 
 "---"
